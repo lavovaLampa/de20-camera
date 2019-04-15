92c56cbfc1f1c11d3833c4134d76d408f6b4cd1a
Martin Cizmar <betacheer@gmail.com>
2019-04-16 01:49:40 +0200

    refactor & clean up reset controller

--- src/reset_ctrl.vhd
+++ src/reset_ctrl.vhd
@@ -8,44 +8,33 @@ entity reset_ctrl is
     );
     port(
         clkIn, rstAsyncIn : in  std_logic;
-        i2cDoneIn         : in  boolean;
-        rstI2cOut         : out std_logic := '1';
-        rstCtrlOut        : out std_logic := '1';
-        rstCcdOutNeg      : out std_logic := '0'
+        rstOut            : out std_logic := '1';
+        ccdNegRstOut      : out std_logic := '0'
     );
 end entity reset_ctrl;
 
 architecture RTL of reset_ctrl is
-    signal rstCounter          : natural range 0 to counterMax := 0;
-    -- cross-clock domain synchronization (waiting for slower clocked reg)
-    signal sync1, sync2, sync3 : boolean                       := false;
+    signal rstCounter : natural range 0 to counterMax := 0;
 begin
     rstProc : process(clkIn, rstAsyncIn)
     begin
         if rstAsyncIn = '1' then
-            rstCcdOutNeg <= '0';
-            rstI2cOut    <= '1';
-            rstCtrlOut   <= '1';
-            sync1        <= false;
-            sync2        <= false;
-            sync3        <= false;
+            ccdNegRstOut <= '0';
+            rstOut       <= '1';
             rstCounter   <= 0;
         elsif rising_edge(clkIn) then
-            sync1 <= i2cDoneIn;
-            sync2 <= sync1;
-            sync3 <= sync2;
-
             if rstCounter < counterMax then
                 rstCounter <= rstCounter + 1;
             end if;
-            if rstCounter > counterMax / 10 then
-                rstCcdOutNeg <= '1';
+
+            -- change timing after SDRAM ctrl addition
+            -- bring up digital supplies first
+            if rstCounter > 1000 then
+                rstOut <= '0';
             end if;
-            if rstCounter > counterMax / 2 then
-                rstI2cOut <= '0';
-            end if;
-            if sync2 and sync3 then
-                rstCtrlOut <= '0';
+            -- start ccd
+            if rstCounter > 1500 then
+                ccdNegRstOut <= '1';
             end if;
         end if;
 
