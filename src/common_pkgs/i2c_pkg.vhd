9c868d57334c6a3ec4959e5742df8a5e99687955
Martin Cizmar <betacheer@gmail.com>
2019-04-16 02:20:34 +0200

    start i2c controller + package refactoring

--- src/common_pkgs/i2c_pkg.vhd
+++ src/common_pkgs/i2c_pkg.vhd
@@ -2,24 +2,30 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 package i2c_pkg is
-    constant DATA_WIDTH     : natural                                     := 16;
-    constant ADDR_WIDTH     : natural                                     := 8;
-    constant I2C_WRITE      : std_logic                                   := '0';
-    constant I2C_READ       : std_logic                                   := '1';
-    constant CCD_READ_ADDR  : std_logic_vector((ADDR_WIDTH - 1) downto 0) := X"BB";
-    constant CCD_WRITE_ADDR : std_logic_vector((ADDR_WIDTH - 1) downto 0) := X"BA";
+    constant DATA_WIDTH : natural   := 16;
+    constant ADDR_WIDTH : natural   := 8;
+    constant I2C_WRITE  : std_logic := '0';
+    constant I2C_READ   : std_logic := '1';
 
-    constant FULL_WIDTH : natural := (ADDR_WIDTH * 2) + DATA_WIDTH;
+    subtype Data_Range is natural range DATA_WIDTH - 1 downto 0;
+    subtype Addr_Range is natural range ADDR_WIDTH - 1 downto 0;
 
-    type FSM_State is (WaitForEnable, SendData, SendStop);
+    subtype I2c_Data is std_logic_vector(Data_Range);
+    subtype I2c_Addr is std_logic_vector(Addr_Range);
+
+    constant CCD_READ_ADDR  : I2c_Addr := X"BB";
+    constant CCD_WRITE_ADDR : I2c_Addr := X"BA";
+    constant FULL_WIDTH     : natural  := (ADDR_WIDTH * 2) + DATA_WIDTH;
+
+    type FSM_State is (Ready, SendData, SendStop);
     type ACK_State is (WaitForAck, ReleaseLine, ReadAck);
 
-    function logicToI2CBusState(val : std_logic) return std_logic;
+    pure function logicToI2CBusState(val : std_logic) return std_logic;
 end package i2c_pkg;
 
 package body i2c_pkg is
 
-    function logicToI2CBusState(val : std_logic)
+    pure function logicToI2CBusState(val : std_logic)
     return std_logic is
     begin
         case val is
--- src/i2c_ctrl.vhd
+++ src/i2c_ctrl.vhd
@@ -8,10 +8,11 @@ entity i2c_ctrl is
     port(
         -- TODO: really, check timing information?
         -- clkIn has to be in range of 10s of kHz (for i2c communication) -> 20kHz 
+        -- clkIn has to have 2 * sClkOut HZ (because we change sClk on each rising edge only)
         clkIn, rstAsyncIn     : in    std_logic;
         enableIn              : in    boolean;
-        dataIn                : in    std_logic_vector((DATA_WIDTH - 1) downto 0);
-        devAddrIn, dataAddrIn : in    std_logic_vector((ADDR_WIDTH - 1) downto 0);
+        dataIn                : in    I2c_Data;
+        devAddrIn, dataAddrIn : in    I2c_Addr;
         doneOut               : out   boolean   := false;
         sClkOut               : out   std_logic := '1';
         sDataIO               : inout std_logic := 'Z' -- 1 == 'Z', 0 == '0' (i2c spec)
@@ -19,44 +20,46 @@ entity i2c_ctrl is
 end entity i2c_ctrl;
 
 architecture behavioral of i2c_ctrl is
-    signal fsmState      : FSM_State                                 := WaitForEnable;
+    signal fsmState      : FSM_State                                 := Ready;
     signal ackState      : ACK_State                                 := WaitForAck;
     -- data/addr array pointer
     signal bitIndex      : natural range FULL_WIDTH - 1 downto 0     := FULL_WIDTH - 1;
     signal byteCounter   : natural range 7 downto 0                  := 7;
     signal dataAggregate : std_logic_vector(FULL_WIDTH - 1 downto 0) := X"00000000";
+    signal clkState      : std_logic                                 := '1';
 begin
     controlProc : process(clkIn, rstAsyncIn)
-        variable dataOut  : std_logic := '1';
-        variable forceClk : std_logic := '1';
+        variable dataOut : std_logic := '1';
     begin
         if rstAsyncIn = '1' then
             dataAggregate <= X"00000000";
-            fsmState      <= WaitForEnable;
+            fsmState      <= Ready;
             ackState      <= WaitForAck;
             bitIndex      <= ADDR_WIDTH - 1;
             byteCounter   <= 7;
             doneOut       <= false;
+            -- i2c is idle if SDA = SCL = '1'
+            clkState      <= '1';
             dataOut       := '1';
-            forceClk      := '1';
-        elsif falling_edge(clkIn) then
+        elsif rising_edge(clkIn) then
             case fsmState is
-                when WaitForEnable =>
+                when Ready =>
                     if enableIn then
                         dataAggregate <= devAddrIn & dataAddrIn & dataIn;
                         fsmState      <= SendData;
                         bitIndex      <= ADDR_WIDTH - 1;
                         ackState      <= WaitForAck;
                         byteCounter   <= 7;
+                        -- initiate with a start bit (pull SDA low while clock is high)
                         dataOut       := '0';
-                        forceClk      := '1';
                     end if;
+
                 when SendData =>
-                    forceClk := '0';
-                    dataOut  := dataAggregate(bitIndex);
+                    dataOut := dataAggregate(bitIndex);
                     if bitIndex > 0 then
                         bitIndex <= bitIndex - 1;
                     end if;
+
                     if byteCounter > 0 then
                         byteCounter <= byteCounter - 1;
                     elsif byteCounter = 0 then
@@ -67,7 +70,6 @@ begin
                             ackState <= ReadAck;
                         elsif ackState = ReadAck then
                             if bitIndex = 0 and sDataIO = '0' then
-                                forceClk := '1';
                                 dataOut  := '0';
                                 fsmState <= SendStop;
                             elsif byteCounter = 0 and sDataIO = '0' then
@@ -75,21 +77,21 @@ begin
                                 byteCounter <= 7;
                             else
                                 -- TODO: implement error recovery
-                                fsmState <= WaitForEnable;
+                                fsmState <= Ready;
                             end if;
                         end if;
                     end if;
+
                 when SendStop =>
                     dataOut  := '1';
-                    fsmState <= WaitForEnable;
+                    fsmState <= Ready;
                     doneOut  <= true;
             end case;
         end if;
 
-        sDataIO <= logicToI2CBusState(dataOut);
-
         -- i2c communication
-        sClkOut <= clkIn or forceClk;
+        sDataIO <= logicToI2CBusState(dataOut);
+        sClkOut <= clkState;
 
     end process controlProc;
 end architecture behavioral;
