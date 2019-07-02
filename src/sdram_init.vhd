9db43c2f20becfda182de3735992613613b6307d
Martin Cizmar <betacheer@gmail.com>
2019-07-02 03:51:00 +0200

    initial implementation w/o tests

--- src/sdram_init.vhd
+++ src/sdram_init.vhd
@@ -2,13 +2,21 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use ieee.math_real.all;
+use work.sdram_pkg_new.all;
 
 entity sdram_init is
     generic(
-        CLK_PERIOD : time := 7.5 ns
+        -- FIXME: set sensible defaults
+        CLK_PERIOD  : time                          := 7.5 ns;
+        INIT_DELAY  : time                          := 200 us;
+        tRP         : time                          := 15 ns;
+        tRC         : time                          := 60 ns;
+        tMRD_CYCLES : natural                       := 2;
+        MODE_REG    : std_logic_vector(15 downto 0) := (others => '0')
     );
     port(
         clkIn, rstAsyncIn   : in  std_logic;
+        clkStableIn         : in  std_logic;
         addrOut             : out unsigned(11 downto 0);
         dataOut             : out std_logic_vector(15 downto 0);
         --        bankSelectOut       : out unsigned(1 downto 0);
@@ -16,11 +24,15 @@ entity sdram_init is
         rowAddrStrobeNegOut : out std_logic := '1';
         colAddrStrobeNegOut : out std_logic := '1';
         writeEnableNegOut   : out std_logic := '1';
-        initDoneOut         : out boolean   := false
+        clkEnableOut        : out std_logic := '0';
+        doneOut             : out boolean   := false
     );
-    constant INIT_COUNTER_HIGH : natural := 100 us / CLK_PERIOD;
-    type Init_State_T is (InitDelay, PrechargeAll, AutoRefresh, LoadModeReg, FinalNop, Done);
-    type Cmd_T is (CmdInhibit, NoOp, Active, Read, Write, BurstTerminate, Precharge, Refresh, LoadModeReg);
+    constant tRP_CYCLES          : natural := (tRP / CLK_PERIOD) + 1;
+    constant tRC_CYCLES          : natural := (tRC / CLK_PERIOD) + 1;
+    constant STABLE_POWER_CYCLES : natural := 200;
+    constant INIT_WAIT_CYCLES    : natural := (INIT_DELAY / CLK_PERIOD) + 1;
+    constant COUNTER_LIMIT       : natural := maximum(INIT_WAIT_CYCLES, 2**12);
+    type Init_State_T is (WaitForStablePower, WaitForStableClock, InitDelay, PrechargeAll, AutoRefresh, LoadModeReg, Done);
 end entity sdram_init;
 
 architecture RTL of sdram_init is
@@ -31,16 +43,7 @@ architecture RTL of sdram_init is
     -- wire
     signal cmdSelectAggregate : std_logic_vector(3 downto 0);
 begin
-    with currCmd select cmdSelectAggregate <=
-        "1---" when CmdInhibit,
-        "0111" when NoOp,
-        "0011" when Active,
-        "0101" when Read,
-        "0100" when Write,
-        "0110" when BurstTerminate,
-        "0010" when Precharge,
-        "0001" when Refresh,
-        "0000" when LoadModeReg;
+    cmdSelectAggregate <= encode_cmd(currCmd);
 
     -- decode internal representation to control signals
     chipSelectNegOut    <= cmdSelectAggregate(3);
@@ -49,20 +52,18 @@ begin
     writeEnableNegOut   <= cmdSelectAggregate(0);
 
     initProc : process(clkIn, rstAsyncIn)
-        variable initCounter    : natural range 0 to INIT_COUNTER_HIGH + 100 := 0;
-        variable refreshCounter : natural range 0 to 2**12;
+        variable counter             : natural range 0 to COUNTER_LIMIT := 0;
+        variable auxCounter          : natural range 0 to 10            := 0;
+        variable refreshCycleCounter : natural range 0 to tRC_CYCLES    := 0;
+        variable cmdSent             : boolean                          := false;
 
-        procedure mem_precharge is
+        procedure next_state(nextState : in Init_State_T) is
         begin
-            currCmd <= Precharge;
-            -- precharge all flag
-            addrOut <= b"0000_0000_0010";
-        end procedure mem_precharge;
+            currState  <= nextState;
+            counter    := 0;
+            auxCounter := 0;
+        end procedure next_state;
 
-        procedure mem_refresh is
-        begin
-            currCmd <= Refresh;
-        end procedure mem_refresh;
     begin
         if rstAsyncIn = '1' then
             addrOut             <= (others => '0');
@@ -71,36 +72,89 @@ begin
             rowAddrStrobeNegOut <= '1';
             colAddrStrobeNegOut <= '1';
             writeEnableNegOut   <= '1';
-            initDoneOut         <= false;
+            doneOut             <= false;
+            clkEnableOut        <= '0';
             dataOut             <= (others => 'Z');
 
             -- internal signals/variables
-            currState      <= InitDelay;
-            initCounter    := 0;
-            refreshCounter := 0;
+            currState  <= InitDelay;
+            counter    := 0;
+            auxCounter := 0;
+            cmdSent    := false;
         elsif rising_edge(clkIn) then
             case currState is
+                when WaitForStablePower =>
+                    if counter >= STABLE_POWER_CYCLES then
+                        next_state(WaitForStableClock);
+                    end if;
+
+                when WaitForStableClock =>
+                    if clkStableIn = '1' then
+                        next_state(WaitForStablePower);
+                        clkEnableOut <= '1';
+                    end if;
+
                 when InitDelay =>
-                    if initCounter < INIT_COUNTER_HIGH + 100 then
-                        currState <= PrechargeAll;
-                    else
-                        initCounter := initCounter + 1;
+                    if counter >= INIT_WAIT_CYCLES then
+                        next_state(PrechargeAll);
                     end if;
 
                 when PrechargeAll =>
-                    mem_precharge;
-                    currState <= 
+                    if cmdSent then
+                        currCmd <= NoOp;
+                        if counter > tRP_CYCLES then
+                            next_state(AutoRefresh);
+                        end if;
+                    else
+                        currCmd <= Precharge;
+                        -- enable a10Flag -> Precharge All banks
+                        addrOut <= b"0000_0000_0010";
+                        cmdSent := true;
+                    end if;
 
                 when AutoRefresh =>
-                    null;
-                when LoadModeReg =>
-                    null;
-                when FinalNop =>
-                    null;
-                when Done =>
-                    null;
+                    if cmdSent then
+                        currCmd <= NoOp;
 
+                        if refreshCycleCounter > tRC_CYCLES then
+                            cmdSent             := false;
+                            refreshCycleCounter := 0;
+                        else
+                            refreshCycleCounter := refreshCycleCounter + 1;
+                        end if;
+                    else
+                        if auxCounter > 1 then
+                            next_state(LoadModeReg);
+                        else
+                            currCmd <= Refresh;
+                            cmdSent := true;
+
+                            if counter > 2**12 then
+                                counter    := 0;
+                                auxCounter := auxCounter + 1;
+                            else
+                                counter := counter + 1;
+                            end if;
+                        end if;
+                    end if;
+
+                when LoadModeReg =>
+                    if cmdSent then
+                        currCmd <= NoOp;
+                        if counter > tMRD_CYCLES + 2 then
+                            next_state(Done);
+                        end if;
+                    else
+                        currCmd <= LoadModeReg;
+                        dataOut <= MODE_REG;
+                        cmdSent := true;
+                    end if;
+
+                when Done =>
+                    doneOut <= true;
             end case;
+
+            counter := counter + 1;
         end if;
     end process initProc;
 end architecture RTL;
